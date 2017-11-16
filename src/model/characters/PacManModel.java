8cf94d8773831597683757d70d0c81ffb24603b7
Adilson Vital Junior <adilsonvital2012@gmail.com>
2017-11-16 11:53:27 -0200

    Adiciona update no poder do Pacman

--- src/model/characters/PacManModel.java
+++ src/model/characters/PacManModel.java
@@ -2,10 +2,17 @@ package model.characters;
 
 import model.fruits.FruitModel;
 import java.util.ArrayList;
+import utils.Updatable;
 
-public class PacManModel extends CharacterModel{
+public class PacManModel extends CharacterModel implements Updatable{
     private int score;
     private int lives;
+    private int counter = 0;
+    private boolean powerful;
+
+    
+        
+        
     private ArrayList<FruitModel> fruits;
     
     public PacManModel(double row, double col) {
@@ -13,5 +20,48 @@ public class PacManModel extends CharacterModel{
         score = 0;
         lives = 3;
         fruits = new ArrayList<>();
+        setEatable(false);
+        setRunning(false);
     }
-}
+    
+    
+    public void setPowerful(boolean powerful){
+        this.powerful = powerful;
+    }
+    public boolean getPowerful(){
+        return powerful;
+    }
+    
+    public void updateLives(int lives){
+        this.lives += lives;
+    }
+    public int getLives (){
+        return lives;
+    }
+    
+    public void updateScore (int score){
+        this.score += score;
+    }
+    public int getScore (){
+        return score;
+    }
+
+    @Override
+    public void update() {
+        
+        if(getPowerful() && counter == 720){
+            setPowerful(false);
+            counter = 0;
+            setRealSpeed(0.0625);
+            
+        }else if(getPowerful() && counter != 720){
+            counter++;
+        }
+    }
+        
+    }
+    
+    
+    
+    
+    
